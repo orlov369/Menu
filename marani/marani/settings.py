77728e57b517f76d5052b8670b5e0d58ac1caf05
orlov369 <orlovt220905@gmail.com>
2025-06-05 09:09:39 +0500

    base3

--- marani/marani/settings.py
+++ marani/marani/settings.py
@@ -19,7 +19,7 @@ EMAIL_HOST_USER = 'orlovt220905@gmail.com'  # Полный адрес Gmail
 EMAIL_HOST_PASSWORD = 'dpwk ddqa dcuw zmon'  # Пароль приложения (не пароль от аккаунта!)
 DEFAULT_FROM_EMAIL = 'maranimenu <orlovt220905@gmail.com>'
 
-ALLOWED_HOSTS = ['maranirestoran.ru', 'www.maranirestoran.ru']
+ALLOWED_HOSTS = ['maranirestoran.ru', 'localhost', '127.0.0.1']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
